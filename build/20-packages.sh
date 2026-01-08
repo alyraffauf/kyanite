6d20b8f919295a98d84286bf558eaaeb12d5a330
Aly Raffauf <aly@aly.codes>
2026-01-08 14:41:10 -0500

    add docker + dev packages

--- build/20-packages.sh
+++ build/20-packages.sh
@@ -52,6 +52,20 @@ else
     echo "No packages to remove."
 fi
 
+# Docker packages from their repo
+echo "::group:: Install Docker"
+dnf5 config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/docker-ce.repo
+dnf5 -y install --enablerepo=docker-ce-stable \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose-plugin \
+    docker-model-plugin
+
+echo "::endgroup::"
+
 echo "::group:: Install Cider"
 
 # Import Cider GPG key
