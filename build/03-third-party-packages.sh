199956dd60341c6ea9a86f2ace22b609a4f98ea2
Aly Raffauf <aly@aly.codes>
2026-08-08 10:39:11 -0400

    feat: add development and sync tools

--- build/03-third-party-packages.sh
+++ build/03-third-party-packages.sh
@@ -11,6 +11,8 @@ source /ctx/build/copr-helpers.sh
 ###############################################################################
 # This script optionally installs packages from third-party repositories:
 # - Cider
+# - Terra
+# - Tailscale
 # - COPR repositories
 ###############################################################################
 
@@ -33,9 +35,33 @@ source /ctx/build/copr-helpers.sh
 
 # echo "::endgroup::"
 
-echo "::group:: Install COPR Packages"
+echo "::group:: Install Terra packages"
 
-copr_install_isolated "scottames/ghostty" "ghostty"
+# The bootstrap package is fetched before Terra's signing keys are available.
+# All subsequent Terra packages are verified using the installed keys.
+# shellcheck disable=SC2016 # dnf5 expands $releasever, not Bash.
+TERRA_REPOSITORY='terra,https://repos.fyralabs.com/terra$releasever'
+dnf5 -y install --nogpgcheck \
+    --repofrompath "${TERRA_REPOSITORY}" \
+    terra-release \
+    terra-gpg-keys
+dnf5 config-manager setopt terra.enabled=0
+dnf5 -y install --enablerepo=terra \
+    ghostty \
+    zed
+
+echo "::endgroup::"
+
+echo "::group:: Install Tailscale"
+
+dnf5 config-manager addrepo \
+    --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf5 config-manager setopt tailscale-stable.enabled=0
+dnf5 -y install --enablerepo=tailscale-stable tailscale
+
+echo "::endgroup::"
+
+echo "::group:: Install COPR packages"
 
 # copr_install_isolated "quadratech188/vicinae" "vicinae"
 
